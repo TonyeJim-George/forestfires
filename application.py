1fac92c4fe192b66fb7a1d54791ac54c801d7130
TonyeJim-George <jimgeorgedivine@gmail.com>
2025-04-26 17:13:30 +0100

    first commit

--- application.py
+++ application.py
@@ -1,7 +1,5 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
-import numpy as np
-import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
 application = Flask(__name__)
